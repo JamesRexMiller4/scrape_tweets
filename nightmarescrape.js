44d7792c1d122528b10b487ff84a9fdde0dc9179
James Miller <james.rex.miller.4@gmail.com>
2020-01-26 09:57:54 -0700

    Initial commit

---
+++ nightmarescrape.js
@@ -0,0 +1,10 @@
+// go to twitter url ('https://twitter.com')
+// click and type into <input type='text' 
+// class='text-input email-input' and enter email address
+// click and type into <input type='password' 
+// class='text-input' and enter password
+// click on <a class='StaticLoggedOutHomePage-input
+
+
+{/* <div class="css-1dbjc4n"></div> */}
+// <div class='css-901oao'></div><span></span>
