18bfe797aef609db77d3e8852fd48363a5321845
James Miller <james.rex.miller.4@gmail.com>
2020-01-26 11:01:03 -0700

    Add password file

--- nightmarescrape.js
+++ nightmarescrape.js
@@ -1,14 +1,16 @@
+const password = require('./password');
+
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 
 nightmare
   .goto('https://www.twitter.com')
   .wait(5000)
-  .type('input[name="session[username_or_email]"]', 'MooMooMofucka')
+  .type('input[name="session[username_or_email]"]', `${password.username}`)
   .wait(2000)
   .click('input[type=\'password\']')
   .wait(2000)
-  .type('input[type=\'password\']', 'Buster1991!')
+  .type('input[type=\'password\']', `${password.password}`)
   .click('div.css-18t94o4')
   .wait(8000)
   .evaluate(function (result, done) {
